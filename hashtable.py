67a5298cf634a5f928b4fb70278612052132bbb8
Alan Davis <neptunius@gmail.com>
2016-11-26 14:02:40 -0800

    Add implementation stubs for HashTable class

---
+++ hashtable.py
@@ -0,0 +1,53 @@
+#!python
+
+from linkedlist import LinkedList
+
+
+class HashTable(object):
+
+    def __init__(self, init_size=8):
+        """Initialize this hash table with the given initial size"""
+        self.buckets = [LinkedList() for i in range(init_size)]
+
+    def __repr__(self):
+        """Return a string representation of this hash table"""
+        return 'HashTable({})'.format(self.length())
+
+    def _bucket_index(self, key):
+        """Return the bucket index where the given key would be stored"""
+        return hash(key) % len(self.buckets)
+
+    def length(self):
+        """Return the length of this hash table by traversing its buckets"""
+        # TODO: Count number of key-value entries in each of the buckets
+        pass
+
+    def contains(self):
+        """Return True if this hash table contains the given key, or False"""
+        # TODO: Check if the given key exists in a bucket
+        pass
+
+    def get(self, key):
+        """Return the value associated with the given key, or raise KeyError"""
+        # TODO: Check if the given key exists and return its associated value
+        pass
+
+    def set(self, key, value):
+        """Insert or update the given key with its associated value"""
+        # TODO: Insert or update the given key-value entry into a bucket
+        pass
+
+    def delete(self, key):
+        """Delete the given key from this hash table, or raise KeyError"""
+        # TODO: Find the given key and delete its entry if found
+        pass
+
+    def keys(self):
+        """Return a list of all keys in this hash table"""
+        # TODO: Collect all keys in each of the buckets
+        pass
+
+    def values(self):
+        """Return a list of all values in this hash table"""
+        # TODO: Collect all values in each of the buckets
+        pass
